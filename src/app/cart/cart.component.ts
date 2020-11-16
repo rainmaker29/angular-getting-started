8167be176674bca1168dd565e4e0083bc4de221c
rainmaker29 <amaanrahil29@gmail.com>
2020-11-16 12:49:23 +0530

    Docker

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,7 +7,7 @@ import { CartService } from "../cart.service";
   styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-  iterms;
+  items;
 
   constructor(private cartService: CartService) {}
 
